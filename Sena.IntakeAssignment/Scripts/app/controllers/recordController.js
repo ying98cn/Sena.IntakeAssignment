9f7d89b3774f7fd3ed4729a22cd63afebedc6452
LAPTOP-S2SMR2BQ\ying9 <ying98cn@hotmail.com>
2017-01-28 23:52:18 +0100

    user story 2: Als gebtuiker wil ik in de individuele record kunnen wijzigen zonder de pagina te verlaten. (part1)

--- Sena.IntakeAssignment/Scripts/app/controllers/recordController.js
+++ Sena.IntakeAssignment/Scripts/app/controllers/recordController.js
@@ -8,13 +8,36 @@
     recordController.$inject = ['$scope', 'recordService']; 
 
     function recordController($scope, recordService) {
-        $scope.title = 'recordController';
+        $scope.record = {
+            'artist': 'wewe',
+            'albumTitle': 'ff',
+            'catalogueNumber': 'ff',
+            'title': '',
+            'year': '',
+            'isrcCode': false
+        };
 
+        // load record list
         recordService
             .getRecords()
             .success(function (result) {
                 $scope.RecordList = result;
             });
+
+        // edit record information
+        $scope.editRecord = function () {
+            recordService.updateRecord();
+        }
+
+        // get selected record
+        $scope.getSelectedRecord = function (id) {
+            //var result = 
+            //    $.grep($scope.RecordList, function(e){ 
+            //        return e.id == id; 
+            //    });
+            var result = null;
+            $scope.record = result || $scope.record;
+        }
 
     }
 })();
